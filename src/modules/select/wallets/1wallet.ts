94762647d3fffe3bf5e4b698220ab85834630722
jenya <hypnagonia@gmail.com>
2021-10-02 21:13:06 +0300

    ++

--- src/modules/select/wallets/1wallet.ts
+++ src/modules/select/wallets/1wallet.ts
@@ -10,7 +10,6 @@ import {networkName} from '../../../utilities'
 const getProviderName = () => 'ONE Wallet'
 import * as ONEWallet from '../../../1wallet/iFrame/oneWalletIFrame/index'
 
-
 const wrapRPCResponse = (data: any, result: any) => {
     // console.log('response', {result})
     return {
@@ -23,6 +22,9 @@ const wrapRPCError = (data: any, err: any) => {
     return {'jsonrpc': '2.0', 'id': data.id, 'error': {'code': -32602, 'message': JSON.stringify(err)}}
 
 }
+
+const rpcHarmonyUrl = 'https://api.harmony.one'
+
 
 function oneWallet(
     options: SdkWalletOptions & { networkId: number; rpcUrl: string },
@@ -46,49 +48,205 @@ function oneWallet(
                 provider: {
                     // @ts-ignore
                     send: (data, cb, ...rest) => {
-                            const {method} = data
+                        const {method} = data
 
-                            if (method === 'eth_accounts') {
-                                cb(null, wrapRPCResponse(data, [actualAddress]))
-                                return
-                            }
+                        if (method === 'eth_accounts') {
+                            cb(null, wrapRPCResponse(data, [actualAddress]))
+                            return
+                        }
 
-                            if (method === 'net_version') {
-                                cb(null, wrapRPCResponse(data, '1666600000'))
-                                return
-                            }
+                        if (method === 'net_version') {
+                            cb(null, wrapRPCResponse(data, '1666600000'))
+                            return
+                        }
 
-                            if (method === 'eth_getCode') {
-                                cb(null, wrapRPCResponse(data, '0x1'))
-                                return
-                            }
-
-                            if (method === 'eth_gasPrice') {
-                                cb(null, wrapRPCResponse(data, '0x61a8'))
-                                return
-                            }
+                        if (method === 'eth_gasPrice') {
+                            cb(null, wrapRPCResponse(data, '0x61a8'))
+                            return
+                        }
 
 
-                            if (method === 'eth_sendTransaction') {
-                                const call = async () => {
-                                    let res = []
-                                    for (const p of data.params) {
-                                        const amount = p.value || p.amount || 0
+                        // todo move out rpc calls
+                        if (method === 'eth_getCode') {
+                            // @ts-ignore
+                            const call = async () => {
+                                // console.log('eth_getCode call', data)
+                                try {
+                                    const resRaw = await fetch(rpcHarmonyUrl, {
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                        },
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            ...data,
+                                            params: Array.isArray(data.params[0]) ? data.params[0] : data.params,
+                                        }),
+                                    })
 
-                                        try {
-                                            const r = await ONEWallet.call(p.to, p.data, amount)
-                                            res.push(r)
-                                        } catch (e) {
-                                            return cb(wrapRPCError(data, e), null)
-                                        }
+                                    const res = await resRaw.json()
 
-                                    }
-
-                                    cb(null, wrapRPCResponse(data, res))
+                                    // console.log('eth_getCode success', res)
+                                    cb(null, res)
+                                } catch (e) {
+                                    // console.log('eth_getCode err', e)
+                                    cb(wrapRPCError(data, e), null)
                                 }
 
-                                return call()
                             }
+
+                            return call()
+                        }
+
+                        if (method === 'eth_getTransactionReceipt') {
+                            // @ts-ignore
+                            const call = async () => {
+                                console.log('eth_getTransactionReceipt call', data)
+                                try {
+                                    const resRaw = await fetch(rpcHarmonyUrl, {
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                        },
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            ...data,
+                                            params: Array.isArray(data.params[0]) ? data.params[0] : data.params,
+                                        }),
+                                    })
+
+                                    const res = await resRaw.json()
+
+                                    console.log('eth_getTransactionReceipt success', res)
+                                    cb(null, res)
+                                } catch (e) {
+                                    console.log('eth_getTransactionReceipt err', e)
+                                    cb(wrapRPCError(data, e), null)
+                                }
+
+                            }
+
+                            return call()
+                        }
+
+                        if (method === 'eth_getTransactionByHash') {
+                            // @ts-ignore
+                            const call = async () => {
+                                console.log('eth_getTransactionByHash call', data)
+                                try {
+                                    const resRaw = await fetch(rpcHarmonyUrl, {
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                        },
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            ...data,
+                                            params: Array.isArray(data.params[0]) ? data.params[0] : data.params,
+                                        }),
+                                    })
+
+                                    const res = await resRaw.json()
+
+                                    console.log('eth_getTransactionByHash response', res)
+                                    cb(null, res)
+                                } catch (e) {
+                                    console.log('eth_getTransactionByHash err', e)
+                                    cb(wrapRPCError(data, e), null)
+                                }
+
+                            }
+
+                            return call()
+                        }
+
+                        if (method === 'eth_call') {
+                            // @ts-ignore
+                            const call = async () => {
+                                console.log('eth_call call', data)
+                                try {
+                                    const resRaw = await fetch(rpcHarmonyUrl, {
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                        },
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            ...data,
+                                            params: data.params,
+                                        }),
+                                    })
+
+                                    const res = await resRaw.json()
+
+                                    console.log('eth_call response', res)
+                                    cb(null, res)
+                                } catch (e) {
+                                    console.log('eth_call err', e)
+                                    cb(wrapRPCError(data, e), null)
+                                }
+
+                            }
+
+                            return call()
+                        }
+
+                        if (method === 'eth_getBalance') {
+                            // @ts-ignore
+                            const call = async () => {
+                                console.log('eth_getBalance  call', data)
+                                try {
+                                    const resRaw = await fetch(rpcHarmonyUrl, {
+                                        headers: {
+                                            'Accept': 'application/json',
+                                            'Content-Type': 'application/json',
+                                        },
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            ...data,
+                                            params: data.params,
+                                        }),
+                                    })
+
+                                    const res = await resRaw.json()
+
+                                    console.log('eth_getBalance  response', res)
+                                    cb(null, res)
+                                } catch (e) {
+                                    console.log('eth_getBalance err', e)
+                                    cb(wrapRPCError(data, e), null)
+                                }
+
+                            }
+
+                            return call()
+                        }
+
+
+
+                        if (method === 'eth_sendTransaction') {
+                            const call = async () => {
+                                let res = []
+                                for (const p of data.params) {
+                                    const amount = p.value || p.amount || 0
+
+                                    try {
+                                        const r = await ONEWallet.call(p.to, p.data, amount)
+                                        res.push(r)
+                                    } catch (e) {
+                                        return cb(wrapRPCError(data, e), null)
+                                    }
+
+                                }
+
+                                const r = (res.length && res.length !== 1) ? res : res[0]
+                                cb(null, wrapRPCResponse(data, r))
+                            }
+
+                            return call()
+                        }
+
+                        console.log('not supported',method, data)
 
                     },
                 },
